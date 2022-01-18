1c4af68e296ab1feda73d98835ec678e9611f3eb
wael AL BASHA <wael.al_basha@ensta-bretagne.org>
2022-01-18 16:08:13 +0100

    System Info on server

---
+++ src/SystemInfo.ts
@@ -0,0 +1,60 @@
+const si = require('systeminformation');
+
+// callback style
+si.cpu(function (data) {
+    console.log('CPU-Information:');
+    console.log(data);
+});
+
+// promises style - new in version 3
+si.cpu()
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+
+si.mem()
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+
+si.system()
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+
+si.osInfo()
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+
+si.currentLoad()
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+
+si.processes()
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+
+si.diskLayout()
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+
+si.networkInterfaces()
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+
+// full async / await example (node >= 7.6)
+export async function all_result() {
+    try {
+        const result = [];
+        result.push(await si.cpu());
+        result.push(await si.mem());
+        result.push(await si.system());
+        result.push(await si.osInfo());
+        result.push(await si.currentLoad());
+        result.push(await si.processes());
+        result.push(await si.diskLayout());
+        result.push(await si.networkInterfaces());
+        return result
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+// https://stackoverflow.com/questions/10332565/how-to-get-system-statistics-with-node-js
---
+++ src/server.ts
@@ -0,0 +1,41 @@
+const http = require('http');
+import { all_result } from './SystemInfo'
+
+const hostname = 'localhost';
+var port = 8080;
+
+
+
+const requestListener = async function (req, res) {
+    if (req.url === '/api/v1/sysinfo') {
+        var json = JSON.stringify(await all_result())
+        res.writeHead(200, { 'Content-type': 'application/json' })
+        res.write(json);
+        res.end();
+    }
+    else {
+        res.writeHead(200, { 'Content-type': 'application/json' })
+        res.write("Page not found (404)");
+    }
+
+}
+
+const server = http.createServer(requestListener);
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/api`);
+});
+
+// var http = require('http');
+
+// var server = http.createServer(function (req, res) {
+
+//     if (req.url == '/data') { //check the URL of the current request
+//         res.writeHead(200, { 'Content-Type': 'application/json' });
+//         res.write(JSON.stringify({ message: "Hello World" }));
+//         res.end();
+//     }
+// });
+
+// server.listen(5000);
+
+// console.log('Node.js web server at port 5000 is running..')
