bea4031d9c058e67a71453ac76650790fe6ed489
wael AL BASHA <wael.al_basha@ensta-bretagne.org>
2022-01-29 20:02:35 +0100

    Process.env.PORT

--- src/server.ts
+++ src/server.ts
@@ -21,7 +21,7 @@ const requestListener = async function (req, res) {
 }
 
 const server = http.createServer(requestListener);
-server.listen(port, hostname, () => {
+server.listen(port || process.env.PORT, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/api/v1/sysinfo`);
 });
 
