ac84bc136fdad6c75ae57256713fad6fa2a6bef0
wael AL BASHA <wael.al_basha@ensta-bretagne.org>
2022-01-30 22:53:37 +0100

    test a modif

--- src/SystemInfo.ts
+++ src/SystemInfo.ts
@@ -1,43 +1,43 @@
 const si = require('systeminformation');
 
 // callback style
-si.cpu(function (data) {
-    console.log('CPU-Information:');
-    console.log(data);
-});
+// si.cpu(function (data) {
+//     console.log('CPU-Information:');
+//     console.log(data);
+// });
 
-// promises style - new in version 3
-si.cpu()
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
+// // promises style - new in version 3
+// si.cpu()
+//     .then(data => console.log(data))
+//     .catch(error => console.error(error));
 
-si.mem()
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
+// si.mem()
+//     .then(data => console.log(data))
+//     .catch(error => console.error(error));
 
-si.system()
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
+// si.system()
+//     .then(data => console.log(data))
+//     .catch(error => console.error(error));
 
-si.osInfo()
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
+// si.osInfo()
+//     .then(data => console.log(data))
+//     .catch(error => console.error(error));
 
-si.currentLoad()
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
+// si.currentLoad()
+//     .then(data => console.log(data))
+//     .catch(error => console.error(error));
 
-si.processes()
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
+// si.processes()
+//     .then(data => console.log(data))
+//     .catch(error => console.error(error));
 
-si.diskLayout()
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
+// si.diskLayout()
+//     .then(data => console.log(data))
+//     .catch(error => console.error(error));
 
-si.networkInterfaces()
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
+// si.networkInterfaces()
+//     .then(data => console.log(data))
+//     .catch(error => console.error(error));
 
 // full async / await example (node >= 7.6)
 export async function all_result() {
